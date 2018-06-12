88425be6560dcf9c88ce1e43a74c072d1f19380a
Vinay Raj <31814654+vinay212@users.noreply.github.com>
2018-06-12 14:39:33 +0530

    Dev-2 fixed

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!12345678910TRAIN" );
+        System.out.println( "Hello World!1234567" );
     }
 }
